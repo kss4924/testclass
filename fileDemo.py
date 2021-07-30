11278ffc79ce5611064f336643b005d8c5a18ab6
kss4924 <88189520+kss4924@users.noreply.github.com>
2021-07-30 19:22:06 +0900

    Add files via upload

---
+++ fileDemo.py
@@ -0,0 +1,14 @@
+#file = open('myfile.txt','wt')
+#print('myFile.txt was created ')
+#file.close()
+
+
+#with open('myfile2.txt','wt') as file:
+#    print('myfile2.txt was created')
+
+file = open('hello.txt','at')
+
+file.write('Hello.\n')
+file.write('Nice 2 meet u. \n')
+print('created')
+file.close()
